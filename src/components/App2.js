ab97f43ff06fa53bbcb8331697a786a6db639d1a
JamesMoabi <jamesmoabi8@gmail.com>
2024-07-12 14:59:13 +0200

    Yah

--- src/components/App2.js
+++ src/components/App2.js
@@ -7,9 +7,8 @@ function Form() {
   const [filteredEmployees,setFilteredEmployees]=useState([]);
   const [newEmployee, setNewEmployee]=useState({
     name: '', gender: '', email: '',
-    phone: '', image: '', position: '',
-    id: ''
-  });
+    phone: '', image: '', position: '',id: ''});
+
   const [isEditing,setIsEditing]=useState(false);
   const [currentEmployeeId,setCurrentEmployeeId]=useState('');
   const [errors,setErrors]=useState({});
@@ -18,53 +17,44 @@ function Form() {
     let tempErrors={};
     tempErrors.name=newEmployee.name?"":"This field is required.";
     tempErrors.gender=newEmployee.gender?"":"This field is required.";
-    tempErrors.email=newEmployee.email?(/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(newEmployee.email)?"":"Are you a bot?."):"This field is required.";
+    tempErrors.email=newEmployee.email?(/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(newEmployee.email)?"":"Are you a bot?"):"This field is required.";
     tempErrors.phone=newEmployee.phone?(/^\d{10}$/.test(newEmployee.phone)?"" :"How are we supposed to contact you?"):"This field is required.";
     tempErrors.id=newEmployee.id?"":"Government needs this.";
     setErrors(tempErrors);
-    return Object.values(tempErrors).every(x=>x==="");
-  };
+    return Object.values(tempErrors).every(x=>x==="");};
 
   const addEmployee=()=>{
     if(!validate()) return;
     if(employees.some(employee=>employee.id===newEmployee.id)){
       alert('Congrats you might be a clone.');
-      return;
-    }
-    setEmployees([...employees, newEmployee]);
+      return;}
+
+    setEmployees([...employees,newEmployee]);
     resetForm();
-    alert('Poverty avoided successfully!');
-  };
+    alert('Poverty avoided successfully!');};
 
   const resetForm=()=>{
     setNewEmployee({
       name: '', gender: '', email: '',
-      phone: '', image: '', position: '',
-      id: ''
-    });
-    setIsEditing(false);
-    setCurrentEmployeeId('');
-    setErrors({});
-  };
+      phone: '', image: '', position: '',id: ''});
+    setIsEditing(false);setCurrentEmployeeId('');setErrors({});};
 
   const deleteEmployee=(id)=>{
     setEmployees(employees.filter(employee=>employee.id!==id));};
 
   const editEmployee=(employee)=>{
-    setNewEmployee(employee);
-    setIsEditing(true);
+    setNewEmployee(employee);setIsEditing(true);
     setCurrentEmployeeId(employee.id);};
 
   const updateEmployee=()=>{
     if(!validate()) return;
     setEmployees(employees.map(employee=>(employee.id===currentEmployeeId?newEmployee:employee)));
-    resetForm();
-  };
+    resetForm();};
 
   const handleSubmit=()=>{
     if (isEditing){
-      updateEmployee();
-    }else {addEmployee();}};
+      updateEmployee();}
+      else {addEmployee();}};
 
   const handleSearch=()=>{
     setFilteredEmployees(employees.filter(employee=>employee.name.includes(searchQuery)||employee.id.includes(searchQuery)));};
@@ -83,8 +73,9 @@ function Form() {
 
       <div>
         <h2>{isEditing?'Edit Employee':'Add Employee'}</h2>
-        <input type="text" placeholder="Name?" value={newEmployee.name}
-          onChange={(e) => setNewEmployee({...newEmployee,name:e.target.value})}/>
+        <input type="text" placeholder="Name/Initials?" value={newEmployee.name}
+          onChange={(e)=>setNewEmployee({...newEmployee,name:e.target.value})}/>
+       
         <div className="error">{errors.name}</div>
 
         <input type="email" placeholder="Email?" value={newEmployee.email}
@@ -92,7 +83,7 @@ function Form() {
 
         <div className="error">{errors.email}</div>
 
-        <input type="text" placeholder="Phone?" value={newEmployee.phone}
+        <input type="text" placeholder="PhoneNo?" value={newEmployee.phone}
           onChange={(e)=>setNewEmployee({...newEmployee,phone:e.target.value})}/>
         
         <div className="error">{errors.phone}</div>
@@ -110,16 +101,17 @@ function Form() {
 
         <input type="text" placeholder="ID?" value={newEmployee.id}
           onChange={(e)=>setNewEmployee({...newEmployee,id:e.target.value})}/>
+        
         <div className="error">{errors.id}</div>
 
-        <button onClick={handleSubmit}>{isEditing?'Update Employee':'Add Employee'}</button>
+        <button onClick={handleSubmit}>{isEditing?'Update info?':'Submit Info?'}</button>
         {isEditing &&<button onClick={resetForm}>Cancel</button>}
       </div>
 
-      <div>
+      <div class="table-container">
         <h2>Employee List</h2>
         <table>
-          <thead>
+        <thead>
             <tr>
               <th>Name</th>
               <th>Email</th>
@@ -129,8 +121,8 @@ function Form() {
               <th>ID</th>
               <th>Actions</th>
             </tr>
-          </thead>
-          <tbody>
+        </thead>
+        <tbody>
             {(searchQuery?filteredEmployees:employees)
             .map(employee=>(
               <tr key={employee.id}>
@@ -146,7 +138,7 @@ function Form() {
                 </td>
               </tr>
             ))}
-          </tbody>
+        </tbody>
         </table>
       </div>
     </div>
